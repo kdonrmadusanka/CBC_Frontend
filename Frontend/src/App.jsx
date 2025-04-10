a9cf12bfd50f59a1573cfe0bb1d480a1b3c2fb9b
kdonrmadusanka <ravmkdon@gmail.com>
2025-04-10 21:35:47 +0530

    Intialize a Vite Project.

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Products from './components/Products/Products'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
